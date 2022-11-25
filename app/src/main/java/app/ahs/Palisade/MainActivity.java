e1bce9de2d5073b62b08b568c0a2b97a6bca11bb
Jc-965 <jesse58300@gmail.com>
2022-11-24 22:18:14 -0800

    Added gui

--- app/src/main/java/app/ahs/Palisade/MainActivity.java
+++ app/src/main/java/app/ahs/Palisade/MainActivity.java
@@ -2,6 +2,7 @@ package app.ahs.Palisade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_scene);
+
+        TextView textView
 
     }
 }
