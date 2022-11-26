1d71be9976c343875cf2d717ecb9f60140a2f930
Aiden K <109487996+Icethetea@users.noreply.github.com>
2022-11-25 17:20:28 -0800

    touchups on ui and firebase

---
+++ app/src/main/java/app/ahs/Palisade/TopicMenu.java
@@ -0,0 +1,13 @@
+package app.ahs.Palisade;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TopicMenu extends AppCompatActivity {
+
+    //First topic
+    public void clicked(View view) {
+
+    }
+}
