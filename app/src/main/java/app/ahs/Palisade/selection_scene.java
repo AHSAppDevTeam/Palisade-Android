ba8eac3170c95a64298b052b0ad2c7ed2b88721e
Aiden K <109487996+Icethetea@users.noreply.github.com>
2022-11-27 17:45:20 -0800

    Bug fix for splash/loading screen

--- app/src/main/java/app/ahs/Palisade/selection_scene.java
+++ app/src/main/java/app/ahs/Palisade/selection_scene.java
@@ -5,49 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 import java.util.Objects;
 
 public class selection_scene extends AppCompatActivity implements View.OnClickListener {
 
-
-
+    MaterialToolbar textchange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selection_scene);
         Objects.requireNonNull(getSupportActionBar()).hide();
+        textchange = findViewById(R.id.topAppBar);
     }
 
-    TextView textchange = findViewById(R.id.topAppBar);
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.nature_button:
-                textchange.setText((String) "Nature");
+                textchange.setTitle((String) "Nature");
                 break;
             case R.id.sports_button:
-                textchange.setText((String) "Sports");
+                textchange.setTitle((String) "Sports");
                 break;
             case R.id.art_button:
-                textchange.setText((String) "Art");
+                textchange.setTitle((String) "Art");
                 break;
             case R.id.dating_button:
-                textchange.setText((String) "Dating");
+                textchange.setTitle((String) "Dating");
                 break;
             case R.id.feelings_button:
-                textchange.setText((String) "Feelings");
+                textchange.setTitle((String) "Feelings");
                 break;
             case R.id.food_button:
-                textchange.setText((String) "Food");
+                textchange.setTitle((String) "Food");
                 break;
             case R.id.gaming_button:
-                textchange.setText((String) "Gaming");
+                textchange.setTitle((String) "Gaming");
                 break;
             case R.id.work_button:
-                textchange.setText((String) "Work");
+                textchange.setTitle((String) "Work");
                 break;
         }
         Intent i = new Intent(this, posts_menu.class);
